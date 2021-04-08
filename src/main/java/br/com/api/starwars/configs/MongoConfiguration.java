59054974c93087e119dfa6df3ce6889aff65ce64
Audinéia Carmo <audineia.carmo@ilia.digital>
2021-04-07 21:33:22 -0300

    :recycle: Creating packages

---
+++ src/main/java/br/com/api/starwars/configs/MongoConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.api.starwars.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfiguration {
+
+}
--- src/test/java/br/com/api/starwars/StarWarsApplicationTests.java
+++ src/test/java/br/com/api/starwars/StarWarsApplicationTests.java
@@ -1,12 +1,10 @@
 package br.com.api.starwars;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StarWarsApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
